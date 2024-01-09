16857c3874bd62d00f902c0851c3074f4e94040e
Adauto Júnior <adautodcjunior@gmail.com>
2024-01-09 07:16:57 -0300

    feat: finished lesson 05 of the "Symfony Framework: criando uma aplicação com MVC" course

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -2,22 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\SeriesEntity;
+use App\Repository\SeriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SeriesController extends AbstractController
 {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+
+    }
+
     #[Route('/series', 'app_series', methods: ['GET'])]
     public function getAll(): Response
     {
-        $seriesList = [
-            'LoKi',
-            'Game of Thrones',
-            'Suits',
-            'Upload',
-            'Friends',
-        ];
+        $seriesList = $this->seriesRepository->findAll();
 
         return $this->render('series/index.html.twig', [
             'seriesList' => $seriesList,
@@ -25,8 +28,19 @@ class SeriesController extends AbstractController
     }
 
     #[Route('/series/create', methods: ['GET'])]
-    public function addForm(): Response
+    public function addSeriesForm(): Response
     {
         return $this->render('series/form.html.twig');
+    }
+
+    #[Route('/series/create', methods: ['POST'])]
+    public function addSeries(Request $request): RedirectResponse
+    {
+        $seriesName = $request->request->get('name');
+        $series = new SeriesEntity($seriesName);
+
+        $this->seriesRepository->add($series, true);
+
+        return new RedirectResponse('/series');
     }
 }
---
+++ src/Entity/SeriesEntity.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SeriesRepository;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity(SeriesRepository::class)]
+#[Table('series')]
+class SeriesEntity
+{
+    #[Id, GeneratedValue, Column]
+    private int $id;
+
+    public function __construct(
+        #[Column]
+        private string $name
+    ) { }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $newName): void
+    {
+        $this->name = $newName;
+    }
+}
