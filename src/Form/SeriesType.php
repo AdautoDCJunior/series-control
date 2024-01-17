9d9f2be9843aefe27e608cef3961e48d38fc5919
Adauto Júnior <adautodcjunior@gmail.com>
2024-01-17 07:03:22 -0300

    feat: finished lesson 05 of the "Symfony Framework: formulários, validação e sessão" course

--- src/Form/SeriesType.php
+++ src/Form/SeriesType.php
@@ -3,7 +3,6 @@
 use App\Entity\SeriesEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +11,20 @@ class SeriesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, ['label' => 'Nome'])
-            ->add('save', SubmitType::class, ['label' => 'Adicionar']);
+            ->add('name', options: ['label' => 'Nome:'])
+            ->add('save', SubmitType::class, [
+                'label' => $options['is_edit'] ? 'Editar' : 'Adicionar'
+            ])
+            ->setMethod($options['is_edit'] ? 'PUT' : 'POST');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => SeriesEntity::class,
+            'is_edit' => false,
         ]);
+
+        $resolver->setAllowedTypes('is_edit', 'bool');
     }
 }
